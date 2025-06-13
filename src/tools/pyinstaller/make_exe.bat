a79e7f8e59e3bfc946aa1f69b85ce1aef444db9c
kinefuchi <kinefuchi@skin.jp>
2025-06-13 16:19:30 +0900

    pyinstaller、調整中

--- src/tools/pyinstaller/make_exe.bat
+++ src/tools/pyinstaller/make_exe.bat
@@ -1,2 +1,14 @@
-rem pyinstaller --onefile --noconsole --name=aii_ocr_client main.py
-rem pyinstaller --onefile             --name=aii_ocr_client main.py
+# corporate.cerをmain.pyと同じフォルダに置いた場合
+# Windowsでは区切り文字に ; を使います
+# pyinstaller --name=aii_ocr_client main.py --onefile --additional-hooks-dir=../hooks/ --add-data "dx-suite.crt;."
+
+
+REM pyinstaller --name=aii_ocr_client main.py --additional-hooks-dir=../hooks --onefile
+
+rem TEST
+rem pyinstaller --name=aii_ocr_client main.py --additional-hooks-dir=../hooks
+
+rem pyinstaller --name=aii_ocr_client main.py --additional-hooks-dir=../hooks --onefile
+
+rem pyinstaller --name=aii_ocr_client main.py --additional-hooks-dir=../hooks --onefile
+rem pyinstaller --name=aii_ocr_client main.py --additional-hooks-dir=../hooks --onefile --noconsole
