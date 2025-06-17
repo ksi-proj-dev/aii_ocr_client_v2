58c159a5f8c86cbcb2950c4a6b9e62e6d89ba6b1
kinefuchi <kinefuchi@skin.jp>
2025-06-17 13:37:45 +0900

    pyinstaaller設定変更

--- src/tools/pyinstaller/make_exe.bat
+++ src/tools/pyinstaller/make_exe.bat
@@ -1,3 +1,6 @@
+pyinstaller --name=aii_ocr_client main.py --onefile  --noconsole
+
+
 # corporate.cerをmain.pyと同じフォルダに置いた場合
 # Windowsでは区切り文字に ; を使います
 # pyinstaller --name=aii_ocr_client main.py --onefile --additional-hooks-dir=../hooks/ --add-data "dx-suite.crt;."
